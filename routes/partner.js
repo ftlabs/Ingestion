7d9b16882e26a279a391697c2bd890faee0c0767
Sean Tracey <seanmtracey@gmail.com>
2016-08-09 11:05:32 +0100

    Let's get those endpoints and templates set up

---
+++ routes/partner.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+  res.render('content', { title: 'FT Content' });
+});
+
+router.get('/:uuid', function(req, res, next) {
+//   res.render('content', { title: 'FT Content' });
+	res.send('OK');
+});
+
+module.exports = router;
