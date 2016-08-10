811272520514422b97c08ad8701920b12d287538
Sean Tracey <seanmtracey@gmail.com>
2016-08-10 16:28:43 +0100

    Use PASS, not PATH

--- bin/lib/checkcreds.js
+++ bin/lib/checkcreds.js
@@ -4,7 +4,7 @@ module.exports = function(req, res){
 
 	const credentials = auth(req);
 
-	if (!credentials || credentials.name !== process.env.BASIC_AUTH_USER || credentials.pass !== process.env.BASIC_AUTH_PATH) {
+	if (!credentials || credentials.name !== process.env.BASIC_AUTH_USER || credentials.pass !== process.env.BASIC_AUTH_PASS) {
 		res.statusCode = 401
 		res.setHeader('WWW-Authenticate', 'Basic realm="FT Partner"');
 		res.end('Access denied');
