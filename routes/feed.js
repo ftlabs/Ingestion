ff2a7e9e8f4f7938c293b0390b5d25efc50aebbe
Sean Tracey <seanmtracey@gmail.com>
2016-08-10 11:11:53 +0100

    Can now add articleUUID to Mongo database

--- routes/feed.js
+++ routes/feed.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const isUUID = require('is-uuid');
 const debug = require('debug')('routes:partner');
+const MongoClient = require('mongodb').MongoClient;
 
 const validCredentials = require('../bin/lib/checkcreds');
 const getContent = require('../bin/lib/content');
@@ -15,23 +16,36 @@ router.get('/all', function(req, res, next){
 
 	if(validCredentials(req, res)){
 
-		const articles = [
-			getContent('48235c16-5e1d-11e6-bb77-a121aa8abd95'),
-			getContent('e5e64860-5df9-11e6-bb77-a121aa8abd95'),
-			getContent('3a2307f0-5d3d-11e6-a72a-bd4bf1198c63'),
-			getContent('f5e79520-5d59-11e6-a72a-bd4bf1198c63'),
-			getContent('b28e83a6-5d86-11e6-a72a-bd4bf1198c63')
-		];
+		const mongoURL = process.env.MONGO_ENDPOINT;
 
-		Promise.all(articles)
-			.then(articles => rssify(articles))
-			.then(XML => {
-				res.send(XML);
+		MongoClient.connect(mongoURL, function(err, db) {
+
+			if(err){
+				console.log(err);
+			}
+
+			var collection = db.collection('articles');
+
+			collection.find({}).toArray(function(err, entries){
+				
+				const articles = entries.map(entry => {
+					return getContent(entry.uuid);
+				});
+
+				Promise.all(articles)
+					.then(articles => rssify(articles))
+					.then(XML => {
+						res.send(XML);
+					})
+					.catch(err => {
+						console.log('Error', err);
+					})
+				;
+
 			})
-			.catch(err => {
-				console.log('Error', err);
-			})
-		;
+
+		});
+
 	}
 
 });
--- routes/ft.js
+++ routes/ft.js
@@ -1,12 +1,69 @@
 const express = require('express');
 const router = express.Router();
 const S3O = require('s3o-middleware');
+const isUUID = require('is-uuid');
+const MongoClient = require('mongodb').MongoClient;
 
 router.use(S3O);
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('ft-uuid', { title: 'Visible Articles' });
+
+router.get('/', function(req, res, next){
+
+  res.render('list-exposed-articles', {title : "Accessible Articles"});
+
+});
+
+router.get('/add', function(req, res, next) {
+  res.render('expose-article', { title: 'Expose an article' });
+});
+
+router.post('/add', (req, res, next) => {
+
+  let articleUUID = req.body.uuid;
+
+  let uuidRegex = /([a-f0-9]{8}(?:-[a-f0-9]{4}){3}-[a-f0-9]{12})/i;
+  let matchedUUID = uuidRegex.exec(articleUUID);
+
+  articleUUID = matchedUUID ? matchedUUID[1] : null; 
+
+  console.log("UUID:", articleUUID);
+
+  if(!articleUUID || !isUUID.anyNonNil(articleUUID)){
+    res.send("Not a valid UUID");
+    return
+  } else {
+    
+    const mongoURL = process.env.MONGO_ENDPOINT;
+
+    MongoClient.connect(mongoURL, function(err, db) {
+
+      if(err){
+        console.log(err);
+      }
+      
+      var collection = db.collection('articles');
+      collection.insertOne({
+        uuid : articleUUID
+      }, function(err, result){
+
+        if(err){
+          console.log(err);
+          res.status(500);
+          res.end();
+          return;
+        } else {
+          console.log(articleUUID, 'has been exposed to 3rd parties');
+          res.send("OK");
+        }
+
+
+      })
+
+    });
+
+  }
+
 });
 
 module.exports = router;
